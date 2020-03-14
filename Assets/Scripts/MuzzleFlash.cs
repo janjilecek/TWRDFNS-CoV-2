2516957d9c5150a8373f175d59002f0de656f878
Jan Jilecek <janjilecek@gmail.com>
2020-03-14 18:23:04 +0100

    muzzleflash

---
+++ Assets/Scripts/MuzzleFlash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour {
+    public GameObject flashHolder;
+    public float flashTime;
+    public Light light;
+    void Start()
+    {
+        light = gameObject.GetComponent<Light>();
+        Deactivate();
+    }
+
+    public void Activate()
+    {
+        flashHolder.SetActive(true);
+        light.gameObject.SetActive(true);
+
+        Invoke("Deactivate", flashTime);
+    }
+
+    void Deactivate()
+    {
+        light.gameObject.SetActive(false);
+        flashHolder.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour {
+
+    public Rigidbody mRigidBody;
+    public float forceMin;
+    public float forceMax;
+    float lifetime = 30;
+    float fadetime = 2;
+	// Use this for initialization
+	void Start () {
+        float force = Random.Range(forceMin, forceMax);
+        mRigidBody.AddForce(transform.right * force);
+        mRigidBody.AddTorque(Random.insideUnitSphere * force);
+        StartCoroutine(Fade());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Fade()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        float percent = 0;
+        float fadeSpeed = 1 / fadetime;
+        Destroy(gameObject);
+    }
+}
